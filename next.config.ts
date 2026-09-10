2a1280dbdc6aca124cb3481df5319083ddbd1e32
rch_artin <artinnourafshar1387@gmail.com>
2026-09-10 16:14:20 +0330

    Fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/portfolio",
   productionBrowserSourceMaps: false,
   images: {
     unoptimized: true,
